0cabdf372c8d07cac5dad54fb2a77cd5860114bd
Jebason-Chellakumar <91525477+Jebason-Chellakumar@users.noreply.github.com>
2023-10-24 11:22:34 +0530

    stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-isExitApp = 'pgrep httpd'
-if [[ -n $isExitApp ]]; then
-    service httpd stop
+function isinstalled {
+  if yum list installed "$httpd" >/dev/null 2>&1; then
+    true
+  else
+    false
+  fi
+}
